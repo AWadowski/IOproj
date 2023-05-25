8f415743e0ee2c8ac073b3b3cf628f08da7b1b2e
Adam Wadowski <adam.wadowski@assecods.pl>
2023-05-25 09:54:09 +0200

    implementacje cd

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@
 Student student; 
 
 void uruchomMenuStudenta(Uczelnia& uczelnia, Student& student) {
-    system("cls");
     int wybor;
     // Dodaj logikę i menu dla studenta
     while (true) {
+        
         std::cout << "Menu studenta" << std::endl;
         std::cout << "1. Sprawdz kursy" << std::endl;
         std::cout << "2. Zapisz sie na kurs" << std::endl;
         std::cout << "3. Sprawdz oceny" << std::endl;
         std::cout << "0. Wyloguj sie" << std::endl;
         std::cin >> wybor;
-
+        system("cls");
         switch (wybor) {
             case 1:
                 uczelnia.ListaStudentow[0].SprawdzKursy(); // Przykładowe wywołanie metody dla pierwszego studenta na liście
@@ -36,14 +36,17 @@ void uruchomMenuStudenta(Uczelnia& uczelnia, Student& student) {
     }
 }
 
-void uruchomMenuWykladowcy(Uczelnia& uczelnia) {
+void uruchomMenuWykladowcy(Uczelnia& uczelnia, Wykladowca& wykladowca) {
     system("cls");
     int wybor;
     // Dodaj logikę i menu dla wykładowcy
     while (true) {
+        int ocena;
+        std::string nazwaKursu;
         std::cout << "Menu wykladowcy" << std::endl;
         std::cout << "1. Dodaj kurs" << std::endl;
         std::cout << "2. Dodaj ocene" << std::endl;
+        std::cout << "3. Modyfikuj kurs" << std::endl;
         std::cout << "0. Wyloguj sie" << std::endl;
         std::cin >> wybor;
 
@@ -86,11 +89,12 @@ Student logowanieStudenta(Uczelnia& uczelnia) {
     }
 
     std::cout << "Bledne dane logowania" << std::endl;
+    exit(0);
     return Student();
 }
 
 Wykladowca logowanieWykladowcy(Uczelnia& uczelnia) {
-    system("cls");
+    
     std::string login, haslo;
     int wybor;
 
@@ -108,37 +112,83 @@ Wykladowca logowanieWykladowcy(Uczelnia& uczelnia) {
             return uczelnia.ListaWykladowcow[i];
         }
     }
-
+    system("cls");
     std::cout << "Bledne dane logowania" << std::endl;
+    exit(0);
     return Wykladowca();
+}
+
+void rejestracjaStudenta(Uczelnia& uczelnia) {
+    system("cls");
+    std::string studentName, login, haslo;
+    std::cout << "Rejestracja studenta" << std::endl;
+    std::cout << "Podaj nazwe studenta: ";
+    std::cin >> studentName;
+    std::cout << "Podaj login: ";
+    std::cin >> login;
+    std::cout << "Podaj haslo: ";
+    std::cin >> haslo;
+    Student nowyStudent;
+    nowyStudent.studentName = studentName;
+    nowyStudent.login = login;
+    nowyStudent.haslo = haslo;
+    nowyStudent.id = uczelnia.ListaStudentow.size();
+    uczelnia.ListaStudentow.push_back(nowyStudent);
+    std::cout << "Zarejestrowano nowego studenta." << std::endl;
+}
+
+void rejestracjaWykladowcy(Uczelnia& uczelnia) {
+    system("cls");
+    std::string name, login, haslo;
+    std::cout << "Rejestracja wykladowcy" << std::endl;
+    std::cout << "Podaj nazwe wykladowcy: ";
+    std::cin >> name;
+    std::cout << "Podaj login: ";
+    std::cin >> login;
+    std::cout << "Podaj haslo: ";
+    std::cin >> haslo;
+    Wykladowca nowyWykladowca;
+    nowyWykladowca.name = name;
+    nowyWykladowca.login = login;
+    nowyWykladowca.haslo = haslo;
+    uczelnia.ListaWykladowcow.push_back(nowyWykladowca);
+    std::cout << "Zarejestrowano nowego wykladowce." << std::endl;
 }
 
 void uruchomMenu(Uczelnia& uczelnia) {
     int wybor;
-    system("cls");
     std::cout << "Witaj w systemie uczelni " << uczelnia.nazwa << std::endl;
     std::cout << "Wybierz opcje:" << std::endl;
     std::cout << "1. Zaloguj sie jako student" << std::endl;
     std::cout << "2. Zaloguj sie jako wykladowca" << std::endl;
-    std::cout << "3. Wyjdz" << std::endl;
+    std::cout << "3. Zarejestruj sie jako student" << std::endl;
+    std::cout << "4. Zarejestruj sie jako wykladowca" << std::endl;
+    std::cout << "5. Wyjdz" << std::endl;
     std::cin >> wybor;
     Student zalogowanyStudent;
     Wykladowca zalogowanyWykladowca;
     switch (wybor) {
         case 1:
             zalogowanyStudent = logowanieStudenta(uczelnia);
-            if(zalogowanyStudent.id != 0){
+            if(zalogowanyStudent.id < uczelnia.ListaStudentow.size()){
             uruchomMenuStudenta(uczelnia, zalogowanyStudent);
             }
             break;
         case 2:
             zalogowanyWykladowca = logowanieWykladowcy(uczelnia);
-            //uruchomMenuWykladowcy(uczelnia,wykladowca);
+            uruchomMenuWykladowcy(uczelnia, zalogowanyWykladowca);
             break;
         case 3:
+            rejestracjaStudenta(uczelnia);
+            break;
+        case 4:
+            rejestracjaWykladowcy(uczelnia);
+            break;
+        case 5:
             exit(0);
             break;
         default:
+            system("cls");
             std::cout << "Nie ma takiej opcji" << std::endl;
             break;
     }
